a1c65a39ecd8479a5d0749ae1ae82bf8864f77c9
Karthik Ravikumar(UST <u32543@ust.com>
2025-09-16 10:50:13 +0530

    Code Cleanuo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
-import CustomerFeedback from './components/CustomerFeedback/CustomerFeedback'
 
 function App() {
 
   return (
     <>
       <h1>Customer Feedback</h1>
-      <CustomerFeedback/>
     </>
   )
 }
